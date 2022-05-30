d2cce2544af9de86b4b1fc42803a6149e1051bd9
nisa_ashfiyani <ashfii.yaa22@gmail.com>
2022-05-30 22:47:06 +0700

    wp2-ajax

---
+++ wp2-c-ajax-master/koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$conn_str = "host=john.db.elephantsql.com " .
+"port=5432 " .
+"user=hmuiafrc " .
+"dbname=hmuiafrc " .
+"password=zbE9l1PboWokSJoWtbOhP6oJBhIkPWdm";
+$conn = pg_connect($conn_str);
+
+// if($conn) {
+//     echo "<h3>Koneksi Berhasil</h3>";
+// } else {
+//     echo "<h3>Koneksi GAGAL TERSAMBUNG</h3>";
+// }
+?>
